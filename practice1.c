26952ab3d23d1d480479b4272d33af58928cd251
zilani131 <zilani.cuet@gmail.com>
2022-04-20 23:39:11 +0600

    finding even number

--- practice1.c
+++ practice1.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main(){
-    
+ int n;
+ scanf("%d",&n);
+//  n is also inclusive mean it the even number  is between <=n .= it is added as written inclusive
+ for (int i = 1; i <=n; i++)
+ {
+     if(i%2==0)
+     {
+         printf("%d ",i);
+     }
+ }
+    return 0;
 }
